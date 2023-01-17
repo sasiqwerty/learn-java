4425a97a682cc98a8c3015dc15697d3b4f7db09d
Sarath Sasi <sarathsasi.g@gmail.com>
2023-01-17 11:59:55 +0530

    made some changes

--- programs/learnPrimes.java
+++ programs/learnPrimes.java
@@ -1,13 +1,11 @@
 package programs;
 
-import programs.FileWriterExample;
-
 public class learnPrimes {
 
     public static void main(String[] args) {
-        String output = "test";
+
         int limit = 100000;
-        System.out.println("prime numbers are up to" + limit + ":");
+        System.out.println("prime numbers are up to " + limit + ":");
         for (int i = 2; i <= limit; i++) {
             boolean isPrime = true;
             for (int j = 2; j < i; j++) {
@@ -17,19 +15,8 @@ public class learnPrimes {
                 }
             }
             if (isPrime) {
-                output = (i + " ");
+                System.out.print(i + " ");
             }
-            FileWriter(){
-            try {
-                FileWriter fw = new FileWriter("C:\\users\\sasi-i7\\documents\\learn-java\\programs\\testout.txt");
-                fw.write("Welcome to javaTpoint.");
-                fw.close();
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            System.out.println("Success...");}
         }
-
     }
-
 }
